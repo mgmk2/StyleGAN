88711b3e8fd3d87ddb1e2175f968bf8be11e2efa
mgmk2 <xis.t65@gmail.com>
2019-10-23 13:40:32 +0900

    feat: add mode argument, which switches static mode and dynamic mode

--- train.py
+++ train.py
@@ -5,9 +5,10 @@ from src.model.stylegan.executor import StyleGAN
 parser = argparse.ArgumentParser()
 parser.add_argument('--use_tpu', action='store_true')
 parser.add_argument('--show_mode', default='pause')
+parser.add_argument('--mode', default='dynamic', choices=['dynamic', 'static'])
 pargs = parser.parse_args()
 
 if __name__ == '__main__':
     p = Params()
-    G = StyleGAN(p, use_tpu=pargs.use_tpu, show_mode=pargs.show_mode)
+    G = StyleGAN(p, use_tpu=pargs.use_tpu, mode=pargs.mode, show_mode=pargs.show_mode)
     G.fit()
